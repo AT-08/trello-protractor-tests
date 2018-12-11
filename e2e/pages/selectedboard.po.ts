7dd3f0960d7a85339e8870f031ba01f6a5d12f7a
Rodrigo Menacho <rmenacho21@gmail.com>
2018-12-11 17:49:54 -0400

    adding some methods of delete boad and add list

--- e2e/pages/selectedboard.po.ts
+++ e2e/pages/selectedboard.po.ts
@@ -5,9 +5,18 @@ export class Selectedboard {
 
     locatorShowMenu = by.css('.board-header-btn.mod-show-menu');
     locatorMenuMore = by.css('.board-menu-navigation-item-link.js-open-more');
-    locatorMenuMoreCloseBoard = by.css('.board-menu-navigation-item-link.js-close-board')
+    locatorMenuMoreCloseBoard = by.css('.board-menu-navigation-item-link.js-close-board');
     locatorConfirmCloseBoard = by.css('.js-confirm.full.negate');
     locatorPermanentlyDeleteBoardLink = by.css('.quiet.js-delete');
+
+    locatorAddMember = by.css('.icon-add-member');
+    locatorFindInputText = by.css('.autocomplete-input-container');
+    locatorInvitationMessageInput = by.css('.js-invitation-message');
+    locatorSendInvitationButton = by.css('.autocomplete-btn.primary');
+
+    locatorAddListButton = by.css('.open-add-list.js-open-add-list');
+    locatorListNameInput = by.css('.list-name-input');
+    locatorSaveListNameButton = by.css('.js-save-edit');
 
     async deleteDashBoard() {
         const itemMore = element(this.locatorMenuMore);
@@ -21,4 +30,25 @@ export class Selectedboard {
         await CommonActions.click(itemConfirmCloseBoard);
     }
 
+    async addMember(user, description: string) {
+        const addMemberButton = element(this.locatorAddMember);
+        await CommonActions.click(addMemberButton);
+        const findMemberInputText = element(this.locatorFindInputText);
+        await CommonActions.setValue(findMemberInputText, user.user);
+        const foundName = element(by.cssContainingText('.full-name', user.user));
+        await CommonActions.click(foundName);
+        const invitationMessageInput = element(this.locatorInvitationMessageInput);
+        await CommonActions.setValue(invitationMessageInput, description);
+        const sendInvitationButton = element(this.locatorSendInvitationButton);
+        await CommonActions.click(sendInvitationButton);
+    }
+
+    async addList(listTitle: string) {
+        const addListButton = element(this.locatorAddListButton);
+        await CommonActions.click(addListButton);
+        const listNameInput = element(this.locatorListNameInput);
+        await CommonActions.setValue(listNameInput, listTitle);
+        const saveListNamebutton = element(this.locatorSaveListNameButton);
+        await CommonActions.click(saveListNamebutton);
+    }
 }
